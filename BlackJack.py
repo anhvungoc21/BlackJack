4b750fadbe7db58dfda5173da2f5ca4822c2fd03
anhvungoc21 <93021862+anhvungoc21@users.noreply.github.com>
2022-01-17 16:24:07 -0600

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -1,6 +1,7 @@
-# Global Values
 import random
 from IPython.display import clear_output
+
+# Global Values
 card_ranks = ('Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
 card_values = {'Ace':None, 'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, "Eight": 8, 'Nine': 9, 'Ten': 10,
               'Jack':10, 'Queen':10, 'King':10}
